712b9df3cf9d5a50b1326e37fbcc1d8a6967e92a
leandrovi <leandrofernandesvieira@gmail.com>
2020-03-26 21:12:59 -0300

    feat: define initial structure

---
+++ calculator/src/application/Program.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author leandro
+ */
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+        sc.close();
+    }
+}
