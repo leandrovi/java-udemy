b4cf5afab82732b2bac0e340fea4107ecb9767dd
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-01 19:18:14 -0300

    feat: create Order entity

--- enums/src/application/Main.java
+++ enums/src/application/Main.java
@@ -5,6 +5,10 @@
  */
 package application;
 
+import entities.Order;
+import entities.enums.OrderStatus;
+import java.util.Date;
+
 /**
  *
  * @author leandro
@@ -12,6 +16,10 @@ package application;
 public class Main {
     
     public static void main(String[] args) {
+        
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+        
+        System.out.println(order);
         
     }
     
