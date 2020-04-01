3148094be2be663f5facaf0dc6df00e43fdf7821
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-01 16:11:10 -0300

    feat: testing arrays

---
+++ vectors_arrays/src/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leandro
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        double[] vect = new double[n];
+        
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+        
+        sc.close();
+        
+    }
+    
+}
