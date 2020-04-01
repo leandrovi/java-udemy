5da06ade3ad963f07bf9f942209df088f230e2f0
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-01 19:12:09 -0300

    feat: create enum

---
+++ enums/src/entities/enums/OrderStatus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities.enums;
+
+/**
+ *
+ * @author leandro
+ */
+public enum OrderStatus {
+    
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+    
+}
