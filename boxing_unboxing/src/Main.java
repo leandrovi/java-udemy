5522912ad2f45f6e962df20ecab09272487c5327
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-01 17:31:34 -0300

    feat: testing boxing, unboxing and wrapper classes

---
+++ boxing_unboxing/src/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leandro
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int x = 200;
+        
+        Object obj = x;
+        
+        System.out.println(obj);
+        
+        int y = (int) obj;
+        
+        System.out.println(y);
+        
+        // Wrapper classes
+        
+        int a = 300;
+        
+        Integer obj2 = a;
+        
+        System.out.println(obj2);
+        
+        int b = obj2;
+        
+        System.out.println(b);
+        
+    }
+    
+}
