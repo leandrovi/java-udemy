d84d7859251bee93c9a39717be9d70c553ba5967
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-01 19:20:56 -0300

    feat: testing enums

--- enums/src/application/Main.java
+++ enums/src/application/Main.java
@@ -21,6 +21,13 @@ public class Main {
         
         System.out.println(order);
         
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        
+        System.out.println(os1);
+        System.out.println(os2);
+        
     }
     
 }
