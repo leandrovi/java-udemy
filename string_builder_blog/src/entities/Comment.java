d977438fc4ad5f8ad58432ce8a12f9a563362656
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-02 17:06:24 -0300

    feat: comment entity

---
+++ string_builder_blog/src/entities/Comment.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author leandro
+ */
+public class Comment {
+    
+    private String text;
+    
+    public Comment() {}
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+}
